bc7507b9152dde43686ef4fabd6b8c9e9fe9bf7b
William Watkinson <harry@revmo.app>
2022-09-18 11:07:42 +0100

    docker added

---
+++ neo4j.sh
@@ -0,0 +1,10 @@
+docker run \
+    --name collatzneo4j \
+    -p7474:7474 -p7687:7687 \
+    -d \
+    -v $HOME/neo4j/data:/data \
+    -v $HOME/neo4j/logs:/logs \
+    -v $HOME/neo4j/import:/var/lib/neo4j/import \
+    -v $HOME/neo4j/plugins:/plugins \
+    --env NEO4J_AUTH=neo4j/none \
+    neo4j:4.4-community
